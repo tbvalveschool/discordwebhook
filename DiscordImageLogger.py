c0f0e3f492a1008e8f62d6a6923b5def2eebc32b
tbvalveschool <theo1758@koldingskoler.dk>
2025-03-17 11:16:00 +0100

    Update DiscordImageLogger.py

--- DiscordImageLogger.py
+++ DiscordImageLogger.py
@@ -2,9 +2,9 @@ import os
 import Discord.py
 import discord
 from flask import Flask
-(Discord_Webhook "https://discord.com/api/webhooks/1351126531367239762/AhAPvNZC2yJqTI9y_I-pk5oIcaeXOpGRYtq7Oj9rcjNmMTb1ina2WjelRMrMkABpixTl")
-(DiscordWebhook "https://discord.com/api/webhooks/1351126531367239762/AhAPvNZC2yJqTI9y_I-pk5oIcaeXOpGRYtq7Oj9rcjNmMTb1ina2WjelRMrMkABpixTl")
-(Discord_Webhook2 "https://discord.com/api/webhooks/1351126531367239762/AhAPvNZC2yJqTI9y_I-pk5oIcaeXOpGRYtq7Oj9rcjNmMTb1ina2WjelRMrMkABpixTl")
-(DiscordWebhook2 "https://discord.com/api/webhooks/1351126531367239762/AhAPvNZC2yJqTI9y_I-pk5oIcaeXOpGRYtq7Oj9rcjNmMTb1ina2WjelRMrMkABpixTl")
+https://discord.com/api/webhooks/1351136430872592499/wTQToHjdqqH60sHbbR-bqZsOa4H6UJfbtiAqWnhIZjmT9xi3qWDfZwMWdmuky6utQ0_7)
+(DiscordWebhook "https://discord.com/api/webhooks/1351136430872592499/wTQToHjdqqH60sHbbR-bqZsOa4H6UJfbtiAqWnhIZjmT9xi3qWDfZwMWdmuky6utQ0_7")
+(Discord_Webhook2 "https://discord.com/api/webhooks/1351136430872592499/wTQToHjdqqH60sHbbR-bqZsOa4H6UJfbtiAqWnhIZjmT9xi3qWDfZwMWdmuky6utQ0_7")
+(DiscordWebhook2 "https://discord.com/api/webhooks/1351136430872592499/wTQToHjdqqH60sHbbR-bqZsOa4H6UJfbtiAqWnhIZjmT9xi3qWDfZwMWdmuky6utQ0_7")
 
 just raid my server heres the webhook link "https://discord.com/api/webhooks/1351126531367239762/AhAPvNZC2yJqTI9y_I-pk5oIcaeXOpGRYtq7Oj9rcjNmMTb1ina2WjelRMrMkABpixTl"
